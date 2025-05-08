3311bde6f2c926b0bfe36702c48cadd4774a989d
soite <mrxtrt416@gmail.com>
2025-05-08 21:13:59 +0330

    thirdOC

--- script.js
+++ script.js
@@ -9,9 +9,23 @@ function getTimeStatus(hour) {
     return "future";
 }
 
+function Loadall() {
+    fetch('http://127.0.0.1:8000/api/tasks/')
+    .then(res => res.json())
+    .then(data => {
+        if(Object.keys(data).length === 0) {
+            tasks = {1 : "", 2 : "",3 : "",4: "",5 : "",6 : "",7 : "",8 : "",9 : "",10: "",11 : "",12 : "",13 : "",14 : "",15 : "",16 : "", 17 : "", 18 : "", 19 : "", 20 : "", 21 : "", 22 : "", 23 : ""};
+        } 
+        else {
+            tasks = data;
+        }
+        loadPlanner();
+    });
+}
+
 function loadPlanner() {
     for(let hour = StartHour; hour <= EndHour; hour++) {
-        const savedTask = localStorage.getItem(`task-${hour}`) || "";
+        const savedTask = tasks[hour] || "";
         const statusClass = getTimeStatus(hour);
 
         const timeBlock = document.createElement("div");
@@ -32,17 +46,21 @@ function loadPlanner() {
         saveBtn.textContent = "save";
         saveBtn.onclick = () => {
             const val = input.value;
-            localStorage.setItem(`task-${hour}`, val);
-            input.disabled = true;
+            fetch('http://127.0.0.1:8000/api/tasks/', {
+                method : 'POST',
+                headers : {'content-type' : 'application/json' },
+                body : JSON.stringify({hour:hour, text:val})
+            }).then(() => {input.disabled = true});
         };
 
         const deleteBtn = document.createElement("button");
         deleteBtn.classList.add("deleteBtn");
         deleteBtn.textContent = "X";
         deleteBtn.onclick = () => {
-            localStorage.removeItem(`task-${hour}`);
-            input.value = "";
-            input.disabled = true;
+            fetch(`http://127.0.0.1:8000/api/tasks/${hour}`, {method : 'DELETE'}).then(() => {
+                input.value = "";
+                input.disabled = true;
+            });
         };
 
         const editBtn = document.createElement("button");
@@ -65,4 +83,4 @@ function formatHour(hour) {
     return `${hour12}${suffix}`;
 }
 
-loadPlanner();
+Loadall();
